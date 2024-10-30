65344b48e0cc2dbdb1f4e82bfdf88c11cdda8ba7
farisbasha <farisbashatm@gmail.com>
2024-10-30 15:55:42 +0530

    Fix package entry points and rebuild

--- index.ts
+++ index.ts
@@ -1,2 +1,4 @@
 export { default as HowinMapsWrapper } from "./src/HowinMapsWrapper";
 export { default as useHowinMaps } from "./src/hooks/useHowinMaps";
+export { default as HowinMap } from "./src/components/HowinMap";
+export { default as AutoComplete } from "./src/components/AutoComplete";
